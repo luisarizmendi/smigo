9abc7f19634579cc4640ea8d90910080b148116a
luisarizmendi <luis.ariz@gmail.com>
2021-03-30 15:37:35 +0200

    Changes for OCP

--- target/ROOT/WEB-INF/js/species/VernacularFilter.js
+++
@@ -1,6 +0,0 @@
-angular.module('smigoModule').filter('vernacular', function vernacularFilter($log, VernacularService) {
-    'use strict';
-    return function (speciesId) {
-        return VernacularService.getVernacular(speciesId).vernacularName;
-    };
-});
